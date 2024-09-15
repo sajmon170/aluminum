0812c283b33606762f15082c3e596ee329e8da48
sajmon <szymon17099@gmail.com>
2024-09-15 20:47:46 +0200

    Removed the line caching optimization

--- p2p-chat/src/ui.rs
+++ p2p-chat/src/ui.rs
@@ -20,34 +20,13 @@ use crate::message::DisplayMessage;
 use crate::eventmanager::PressedKey;
 
 type Term = Terminal<CrosstermBackend<Stdout>>;
-type MaybeLine = Option<(usize, Span)>;
-
-
-#[derive(Debug)]
-struct Span {
-    lo: usize,
-    hi: usize
-}
-
-impl Span {
-    fn from(original: &String, slice: &str) -> Span {
-        let base = original.as_str() as *const str as *const () as usize;
-        let lo = slice as *const str as *const () as usize - base;
-        let hi = lo + slice.len();
-
-        Span { lo, hi }
-    }
-}
-
 
 #[derive(Debug)]
 pub struct AppUI<'a> {
-    area: Rect,
     scroll_pos: u16,
     max_scroll: u16,
     textarea: TextArea<'a>,
     messages: Vec<String>,
-    lines: VecDeque<MaybeLine>,
     must_regenerate: bool
 }
 
@@ -56,43 +35,32 @@ impl<'a> AppUI<'a> {
         let mut textarea = TextArea::default();
         textarea.set_block(Block::bordered());
         textarea.set_cursor_line_style(Style::default());
-        let area = terminal.get_frame().area();
         
         AppUI {
-            area,
             scroll_pos: 0,
             max_scroll: 0,
             textarea,
             must_regenerate: true,
-            lines: VecDeque::new(),
             messages
         }
     }
 
     pub fn draw(&mut self, terminal: &mut Term) -> io::Result<()> {
         terminal.draw(|frame| {
-            let new_area = frame.area();
-            if new_area != self.area {
-                self.must_regenerate = true;
-            }
-            self.area = new_area;
-
             let layout = Layout::default()
                 .direction(Direction::Vertical)
                 .constraints(vec![
                     Constraint::Min(0),
                     Constraint::Length(3),
                 ])
-                .split(self.area);
+                .split(frame.area());
 
-            if self.must_regenerate {
-                self.resize(&layout);
-                self.must_regenerate = false;
+            let items = Self::get_lines(&layout, &self.messages);
+
+            self.max_scroll = items.len() as u16 - layout[0].height;
+            if self.scroll_pos > self.max_scroll {
+                self.scroll_pos = self.max_scroll;
             }
-
-            let items: Vec<Line> = self.lines.iter()
-                .map(|line| Line::from(self.get_slice(line)))
-                .collect();
 
             frame.render_widget(
                 Paragraph::new(Text::from(items))
@@ -108,31 +76,21 @@ impl<'a> AppUI<'a> {
         Ok(())
     }
 
-    fn resize(&mut self, layout: &Rc<[Rect]>) {
-        self.lines = self.messages.iter().zip(0..self.messages.len())
+    fn get_lines<'b>(layout: &Rc<[Rect]>, messages: &'b Vec<String>) -> Vec<Line<'b>> {
+        let mut lines: VecDeque<Line> = messages.iter().zip(0..messages.len())
             .map(|(msg, idx)| {
                 textwrap::wrap(msg, layout[0].width as usize)
                     .into_iter()
-                    .map(move |s| Some((idx, Span::from(msg, s.as_ref()))))
+                    .map(|s| Line::raw(s))
             })
             .flatten()
             .collect();
 
-        while (self.lines.len() as u16) < layout[0].height {
-            self.lines.push_front(None);
+        while (lines.len() as u16) < layout[0].height {
+            lines.push_front(Line::from(""));
         }
-        
-        self.max_scroll = self.lines.len() as u16 - layout[0].height;
-        if self.scroll_pos > self.max_scroll {
-            self.scroll_pos = self.max_scroll;
-        }
-    }
 
-    fn get_slice(&self, line: &MaybeLine) -> &str {
-        line.as_ref().map_or(
-            "",
-            |(idx, span)| &self.messages[*idx][span.lo..span.hi]
-        )
+        lines.into()
     }
 
     // TODO: Implement proper message formatting!
